8a5e4f3a909cb2f6e1c2f3a43e9e3441e5f274e9
Ksenya <ksvo2007@mail.ru>
2013-03-10 17:51:25 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HappyDays::Application.config.secret_token = 'a5809499b39e74184996c2ebd588dd6370fd13803cd44e57e5cef91bb0aa97ad2bd58f667fbd098a283ce26be6d1e4a60057ae9ff5616e9c07f9aade232a4151'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HappyDays::Application.config.session_store :cookie_store, key: '_happy_days_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HappyDays::Application.config.session_store :active_record_store
